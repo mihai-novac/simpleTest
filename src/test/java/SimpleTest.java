8b52b73ef96a6b24196b617c2e3a9c7666898836
dshmaya <dshmaya@users.noreply.github.com>
2017-02-19 14:33:42 +0200

    Update SimpleTest.java

--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -21,42 +21,42 @@ public class SimpleTest {
         Assert.assertEquals(1, 1);
     }
 
-    @Test
-    public void alwaysFail1()
-    {
-        Assert.assertEquals(1, 0);    }
+//     @Test
+//     public void alwaysFail1()
+//     {
+//         Assert.assertEquals(1, 0);    }
 
-     @Test
-    public void alwaysFail2()
-    {
-        Assert.assertEquals(1, 0);
-    }
+//      @Test
+//     public void alwaysFail2()
+//     {
+//         Assert.assertEquals(1, 0);
+//     }
 	
-	@Test
-    public void alwaysFail3()
-    {
-        Assert.assertEquals(1, 0);
-    }
+// 	@Test
+//     public void alwaysFail3()
+//     {
+//         Assert.assertEquals(1, 0);
+//     }
 
-	@Test
-    public void sometimesPass1()
-    {
-        Random random = new Random();
-        int randomNumber = random.nextInt(100);
-        boolean smallerThanFiftee =  randomNumber < 50 ? true : false;
-        Assert.assertEquals(smallerThanFiftee, true);
-// 	Assert.assertEquals(true, true);
-    }
+// 	@Test
+//     public void sometimesPass1()
+//     {
+//         Random random = new Random();
+//         int randomNumber = random.nextInt(100);
+//         boolean smallerThanFiftee =  randomNumber < 50 ? true : false;
+//         Assert.assertEquals(smallerThanFiftee, true);
+// // 	Assert.assertEquals(true, true);
+//     }
 
-     @Test
-    public void sometimesPass2()
-    {
-        Random random = new Random();
-        int randomNumber = random.nextInt(100);
-        boolean smallerThanFiftee =  randomNumber < 50 ? true : false;
-        Assert.assertEquals(smallerThanFiftee, true);
-// 	Assert.assertEquals(true, true);
-    }
+//      @Test
+//     public void sometimesPass2()
+//     {
+//         Random random = new Random();
+//         int randomNumber = random.nextInt(100);
+//         boolean smallerThanFiftee =  randomNumber < 50 ? true : false;
+//         Assert.assertEquals(smallerThanFiftee, true);
+// // 	Assert.assertEquals(true, true);
+//     }
 	
 	@Test
 	@Ignore
