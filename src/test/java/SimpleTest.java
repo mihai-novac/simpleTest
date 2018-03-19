bba0bb305d02596cb3cca183072c00bda9694239
kika0811 <riki.azar@hpe.com>
2018-03-19 17:47:29 +0200

    Update SimpleTest.java

--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -1,229 +1,73 @@
-import junit.framework.Assert;
-import org.junit.Ignore;
-import org.junit.Test;
-import java.util.Random;
 
- 
-/**  
- * Created by radiel on 15/11/2016..                         
- */  
-public class SimpleTest {
-	
-   @Test  
-   public void alwaysPass1()
-   {
-       Assert.assertEquals(1, 2);
-   }
+                /**  
+                 * Created by dshmaya on 15/11/2017 ..0
+                */  
+                public class SimpleTest {
+                                
+                   @Test  
+                   public void alwaysPass1()
+                   {
+                       Assert.assertEquals(1, 1);
+                   }
+                
 
-   @Test
-    public void alwaysPass2()
-    {
-        Assert.assertEquals(1, 1);
-    }
-   @Test  
-   public void alwaysPass3()
-   {
-       Assert.assertEquals(1, 1);
-   }
+                   @Test
+                    public void alwaysPass2()
+                    {
+                        Assert.assertEquals(1, 1);
+                    }
+                   @Test  
+                   public void alwaysPass3()
+                   {
+                       Assert.assertEquals(1, 1);
+                   }
+                
 
-  @Test
-    public void alwaysPass4()
-    {
-        Assert.assertEquals(1, 1);
-    }
-   @Test
-   public void alwaysFail5()
-   {
-       Assert.assertEquals(1, 2);
-   }
-@Test
-   public void alwaysFail6()
-   {
-       Assert.assertEquals(1, 2);
-   }
-	 
-   @Test
-   public void alwaysFail7()
-   {
-       Assert.assertEquals(1, 2);
-   }
-@Test
-   public void alwaysFail8()
-   {
-       Assert.assertEquals(1, 2);
-   }
- 
-   @Test
-   public void alwaysFail9()
-   {
-       Assert.assertEquals(1, 2);
-   }
-@Test
-   public void alwaysFail10()
-   {
-       Assert.assertEquals(1, 2);
-   }
- 
+                  @Test
+                    public void alwaysPass4()
+                    {
+                        Assert.assertEquals(1, 1);
+                    }
+                   @Test
+                   public void alwaysFail5()
+                   {
+                       Assert.assertEquals(1, 2);
+                   }
+                
 
-//     @Test
-//     public void alwaysFail1()
-//     {
-//         Assert.assertEquals(1, 0);    }
+                
 
-//      @Test
-//     public void alwaysFail2()
-//     {
-//         Assert.assertEquals(1, 0);
-//     }
-	
-  
-// 	@Test
-//     public void alwaysFail3()
-//     {
-//         Assert.assertEquals(1, 0);
-//     }
+                  @Test
+                    public void alwaysPass6()
+                    {
+                        Assert.assertEquals(1, 1);
+                    }
+                               @Test  
+                   public void alwaysPass7()
+                   {
+                       Assert.assertEquals(1, 1);
+                   }
 
-/*    @Test
-    public void sometimesPass1()
-    {
-        Random random = new Random();
-        int randomNumber = random.nextInt(100);
-        boolean smallerThanFiftee =  randomNumber < 50 ? true : false;
-        Assert.assertEquals(smallerThanFiftee, true);
-// 	Assert.assertEquals(true, true);
-    }
-
-    @Test
-    public void sometimesPass2()
-    {
-        Random random = new Random();
-        int randomNumber = random.nextInt(100);
-        boolean smallerThanFiftee =  randomNumber < 50 ? true : false;
-        Assert.assertEquals(smallerThanFiftee, true);
-// 	Assert.assertEquals(true, true);
-    }*/
-	
-    @Test
-    @Ignore
-    public void ignored1()
-    {
-        Random random = new Random();
-        int randomNumber = random.nextInt(99);
-        boolean smallerThanFiftee =  randomNumber < 50 ? true : false;
-        Assert.assertEquals(smallerThanFiftee, true);	
-    }
-	
-    @Test
-    @Ignore
-    public void ignored2()
-    {
-        Random random = new Random();
-        int randomNumber = random.nextInt(100);
-        boolean smallerThanFiftee =  randomNumber < 50 ? true : false;
-        Assert.assertEquals(smallerThanFiftee, true);
-    }
-}
- 
-
-import junit.framework.Assert;
-import org.junit.Ignore;
-import org.junit.Test;
-import java.util.Random;
-
- 
-/**  
- * Created by radiel on 15/11/2016..                         
- */  
-public class SimpleTest {
-	
-   @Test  
-   public void alwaysPass1()
-   {
-       Assert.assertEquals(1, 2);
-   }
-
-   @Test
-    public void alwaysPass2()
-    {
-        Assert.assertEquals(1, 1);
-    }
-   @Test  
-   public void alwaysPass3()
-   {
-       Assert.assertEquals(1, 1);
-   }
-
-  @Test
-    public void alwaysPass4()
-    {
-        Assert.assertEquals(1, 1);
-    }
-   @Test
-   public void alwaysFail5()   public void alwaysFail51()
-   {
-       Assert.assertEquals(1, 2);
-   }
+                    @Test
+                    @Ignore
+                    public void ignored1()
+                    {
+                        Random random = new Random();
+                        int randomNumber = random.nextInt(99);
+                        boolean smallerThanFiftee =  randomNumber < 50 ? true : false;
+                        Assert.assertEquals(smallerThanFiftee, true);            
+                    }
+                                
+                    @Test
+                    @Ignore
+                    public void ignored2()
+                    {
+                        Random random = new Random();
+                        int randomNumber = random.nextInt(100);
+                        boolean smallerThanFiftee =  randomNumber < 50 ? true : false;
+                        Assert.assertEquals(smallerThanFiftee, true);
+                    }
+                }
 
 
-	
- 
-
-//     @Test
-//     public void alwaysFail1()
-//     {
-//         Assert.assertEquals(1, 0);    }
-
-//      @Test
-//     public void alwaysFail2()
-//     {
-//         Assert.assertEquals(1, 0);
-//     }
-	
-// 	@Test
-//     public void alwaysFail3()
-//     {
-//         Assert.assertEquals(1, 0);
-//     }
-
-/*    @Test
-    public void sometimesPass1()
-    {
-        Random random = new Random();
-        int randomNumber = random.nextInt(100);
-        boolean smallerThanFiftee =  randomNumber < 50 ? true : false;
-        Assert.assertEquals(smallerThanFiftee, true);
-// 	Assert.assertEquals(true, true);
-    }
-
-    @Test
-    public void sometimesPass2()
-    {
-        Random random = new Random();
-        int randomNumber = random.nextInt(100);
-        boolean smallerThanFiftee =  randomNumber < 50 ? true : false;
-        Assert.assertEquals(smallerThanFiftee, true);
-// 	Assert.assertEquals(true, true);
-    }*/
-	
-    @Test
-    @Ignore
-    public void ignored1()
-    {
-        Random random = new Random();
-        int randomNumber = random.nextInt(99);
-        boolean smallerThanFiftee =  randomNumber < 50 ? true : false;
-        Assert.assertEquals(smallerThanFiftee, true);	
-    }
-	
-    @Test
-    @Ignore
-    public void ignored2()
-    {
-        Random random = new Random();
-        int randomNumber = random.nextInt(100);
-        boolean smallerThanFiftee =  randomNumber < 50 ? true : false;
-        Assert.assertEquals(smallerThanFiftee, true);
-    }
-}
- 
 
