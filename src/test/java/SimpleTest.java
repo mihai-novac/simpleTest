3f29ace9a731d59161d700adc6f2d79b640d1bf5
kika0811 <riki.azar@hpe.com>
2018-03-27 13:39:46 +0300

    Add passed test

--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -36,6 +36,13 @@ import junit.framework.Assert;
                     {
                         Assert.assertEquals(1, 2);
                     }
+                  
+                  @Test  
+                   public void alwaysPass4()
+                   {
+                       Assert.assertEquals(1, 1);
+                   }
+                  
                    @Test
                    public void alwaysFail5()
                    {
