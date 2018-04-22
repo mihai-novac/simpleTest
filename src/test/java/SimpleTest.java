708be9211ec31d5d3bb6decf251946f585f8b3c8
kika0811 <riki.azar@hpe.com>
2018-04-22 14:20:06 +0300

    added 2 failed tests

--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -79,7 +79,17 @@ import junit.framework.Assert;
                        Assert.assertEquals(1, 2);
                    }
                  
-                  
+                  @Test
+                   public void alwaysFail13()
+                   {
+                       Assert.assertEquals(1, 2);
+                   }
+                  @Test
+                   public void alwaysFail14()
+                   {
+                       Assert.assertEquals(1, 2);
+                   }
+                 
                     @Test
                     @Ignore
                     public void ignored1()
