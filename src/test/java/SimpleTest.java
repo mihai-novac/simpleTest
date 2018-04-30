51468bfdbe787c52d019b739efe08de13f3b968c
kika0811 <riki.azar@hpe.com>
2018-04-30 15:06:36 +0300

    Remove failed tests

--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -78,17 +78,7 @@ import junit.framework.Assert;
                    {
                        Assert.assertEquals(1, 2);
                    }
-                   @Test
-                   public void alwaysFail14()
-                   {
-                       Assert.assertEquals(1, 2);
-                   }
                   
-                   @Test
-                   public void alwaysFail15()
-                   {
-                       Assert.assertEquals(1, 2);
-                   }
                     @Test
                     @Ignore
                     public void ignored1()
