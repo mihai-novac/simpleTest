c5431b3ca9ec69967c018dc89ceff01c251929cd
iulia-andreicut <iulia.andreicut@hpe.com>
2018-12-09 02:53:35 +0200

    user story #1012

--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -7,6 +7,18 @@ import junit.framework.Assert;
                  * Created by dshmaya on 15/11/2017 ...04
                 */  
                 public class SimpleTest {
+                  public void sometimesPass46()
+    {
+
+         Random random = new Random();
+         int randomNumber = random.nextInt(100);
+         boolean smallerThanFiftee =  randomNumber < 50 ? true : false;
+         Assert.assertEquals(smallerThanFiftee, true);
+        try {
+            Assert.assertEquals(true, true);
+        }catch (NullPointerException w){
+		}
+    }
                                 
                    @Test  
                    public void alwaysPass1()
