16a45b4e1b8cdc046ff98b0c037e03530a5d9bc2
1 <iulia.andreicut@microfocus.com>
2019-12-01 00:13:54 +0200

    Simplemente

--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -26,7 +26,7 @@ import org.junit.Assert;
                    @Test  
                    public void alwaysPass3()
                    {
-                       Assert.assertEquals(1, 1);
+                       Assert.assertEquals(1, 2);
                    }
                   
                   @Test
