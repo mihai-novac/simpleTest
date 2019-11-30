66f0a3551a84ebd359b27927bf4a726efb33dd68
iulia-andreicut <iulia.andreicut@hpe.com>
2019-11-30 17:08:02 +0200

    This is just a simple test

--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -59,7 +59,7 @@ import org.junit.Assert;
                   @Test
                     public void alwaysPass7()
                     {
-                        Assert.assertEquals(1, 1);
+                        Assert.assertEquals(1, 2);
                     }
                  @Test
                    public void alwaysFail81()
