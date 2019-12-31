8accd3d436e105ad6d3e83c4190997962ef9628f
iulia-andreicut <iulia.andreicut@hpe.com>
2019-12-31 13:23:51 +0200

    Update SimpleTest.java

--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -33,7 +33,7 @@ import org.junit.Assert;
                     @Test
                     public void alwaysPassD()
                     {
-                        Assert.assertEquals(1, 9);
+                        Assert.assertEquals(1, 8);
                     }
 
                     @Test
