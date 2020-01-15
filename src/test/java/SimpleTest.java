13f437ec6309c90fca92d2f1de4c0a64f87dff0a
sa@nga sa@nga <sa@nga>
2020-01-15 11:35:32 +0200

    SimpleTest

--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -107,7 +107,7 @@ import org.junit.Assert;
                   @Test
                     public void alwaysPass7()
                     {
-                        Assert.assertEquals(1, 2);
+                        Assert.assertEquals(1, 3);
                     }
                  @Test
                    public void alwaysFail81()
