d8992b04139d62aa1995ed8ae8f40f193fa47374
iulia-andreicut <iulia.andreicut@microfocus.com>
2020-02-19 13:01:30 +0200

    new Branch commit 2

--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -14,7 +14,7 @@ import org.junit.Assert;
                    public void alwaysPass1()
                    {
                     
-                       Assert.assertEquals(1, 1);
+                       Assert.assertEquals(1, 2);
                    }
 			
 		
