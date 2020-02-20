78ab4424138da018a37cfb72fde1e30e059ae26b
Iulia <iulia.andreicut@microfocus.com>
2020-02-20 10:32:50 +0200

    Sim

--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -15,6 +15,8 @@ import org.junit.Assert;
                    {
                     
                        Assert.assertEquals(1, 1);
+                       Assert.assertEquals(1, 1);
+
                    }
 			
 		
