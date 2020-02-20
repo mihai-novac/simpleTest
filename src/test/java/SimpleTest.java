580275a3e684c3c927251ceb3615aa09c3686c23
iulia-andreicut <iulia.andreicut@microfocus.com>
2020-02-20 02:22:20 +0200

    Update SimpleTest.java

--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -15,7 +15,7 @@ import org.junit.Assert;
                    {
                     
                        Assert.assertEquals(1, 1);
-			   Assert.assertEquals(1, 1);
+			   Assert.assertEquals(1, 4);
                    }
 			
 		
