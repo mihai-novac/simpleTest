3760d22d6c66e77cb30313c8f21bd7abc4c49a42
sa@nga sa@nga <sa@nga>
2020-02-23 09:41:22 +0200

    SimpleTest

--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -10,7 +10,7 @@ import java.util.Random;
                  * Created by dshmaya on 15/11/2017 ...04
                 */  
                 public class SimpleTest {
-                    private int a = 4;
+                    private int a = 3;
                     private int b =3;
                  
                                 
