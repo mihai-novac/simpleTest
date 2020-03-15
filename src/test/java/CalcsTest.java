384d6f283365812acea2996337467aa7ad411491
iulia-andreicut <iulia.andreicut@microfocus.com>
2020-03-15 23:41:37 +0200

    defect #958072 Calcs

--- src/test/java/CalcsTest.java
+++ src/test/java/CalcsTest.java
@@ -5,11 +5,11 @@ public class CalcsTest {
     @Test
     public void alwaysFail()
     {
-        Assert.assertEquals(1, 3);
+        Assert.assertEquals(1, 1);
     }
     @Test
     public void alwaysFail1()
     {
-        Assert.assertEquals(1, 4);
+        Assert.assertEquals(1, 3);
     }
 }
