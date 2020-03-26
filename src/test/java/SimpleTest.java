dd1a71aef97ce7ee0b12c9a7087e77a6beb51a1b
iulia-andreicut <iulia.andreicut@microfocus.com>
2020-03-26 10:29:52 +0200

    Update SimpleTest.java

--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -107,7 +107,7 @@ import java.util.Random;
                     @Test
                     public void alwaysPassC()
                     {
-                        Assert.assertEquals(1, 1);
+                        Assert.assertEquals(1, 4);
                     }
                   @Test
                     public void alwaysPass7()
