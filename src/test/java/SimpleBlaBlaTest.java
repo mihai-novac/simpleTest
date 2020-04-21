6204e572e9785f52605e7d244983ab9dc1d86123
iulia-andreicut <iulia.andreicut@microfocus.com>
2020-04-21 18:17:45 +0300

    Update SimpleBlaBlaTest.java

--- src/test/java/SimpleBlaBlaTest.java
+++ src/test/java/SimpleBlaBlaTest.java
@@ -112,7 +112,7 @@ import java.util.Random;
                   @Test
                     public void alwaysPass7()
                     {
-                        Assert.assertEquals(1, 2);
+                        Assert.assertEquals(1, 1);
                     }
                  @Test
                    public void alwaysFail81()
@@ -122,7 +122,7 @@ import java.util.Random;
                   @Test
                    public void alwaysFail91()
                    {
-                       Assert.assertEquals(1, 1);
+                       Assert.assertEquals(1, 2);
                    }
                   @Test
                    public void alwaysFail10()
