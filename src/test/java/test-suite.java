180532b4415ba4e1df5880bbe629eb6e3fd86d68
iulia-andreicut <iulia.andreicut@microfocus.com>
2020-05-20 09:06:56 +0300

    Create test-suite.java

---
+++ src/test/java/test-suite.java
@@ -0,0 +1,15 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class test-suite {
+    @Test
+    public void alwaysFail()
+    {
+        Assert.assertEquals(1, 1);
+    }
+    @Test
+    public void alwaysFail1()
+    {
+        Assert.assertEquals(1, 1);
+    }
+}
