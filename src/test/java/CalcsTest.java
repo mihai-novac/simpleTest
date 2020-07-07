aa27f25e4deff5968055f1da9f339791bedacd55
iulia-andreicut <iulia.andreicut@microfocus.com>
2020-07-07 14:00:55 +0300

    Update CalcsTest.java

--- src/test/java/CalcsTest.java
+++ src/test/java/CalcsTest.java
@@ -5,12 +5,12 @@ public class CalcsTest {
     @Test
     public void alwaysFail()
     {
-        Assert.assertEquals(1, 1);
+        Assert.assertEquals(1, 2);
     }
     @Test
     public void alwaysFail1()
     {
-        Assert.assertEquals(1, 2);
+        Assert.assertEquals(1, 1);
     }
     @Test
     public void alwaysFail2()
