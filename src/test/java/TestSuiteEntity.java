8f77699e290bffdbf069b5e1cd7436d0c8bb95d3
iulia-andreicut <iulia.andreicut@microfocus.com>
2020-07-10 15:09:28 +0300

    Update TestSuiteEntity.java

--- src/test/java/TestSuiteEntity.java
+++ src/test/java/TestSuiteEntity.java
@@ -10,6 +10,11 @@ public class TestSuiteEntity {
     @Test
     public void alwaysFail1()
     {
+        Assert.assertEquals(1, 1);
+    }
+    @Test
+    public void alwaysFail2()
+    {
         Assert.assertEquals(1, 2);
     }
 }
