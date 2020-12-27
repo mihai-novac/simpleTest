501f6fb27a175f13aad7604a2673e646bd9ad337
iulia-andreicut <iulia.andreicut@microfocus.com>
2020-12-27 16:34:15 +0200

    defect #1002

--- src/test/java/TestSuiteEntity.java
+++ src/test/java/TestSuiteEntity.java
@@ -10,7 +10,7 @@ public class TestSuiteEntity {
     @Test
     public void alwaysFail1()
     {
-        Assert.assertEquals(1, 2);
+        Assert.assertEquals(1, 1);
     }
     @Test
     public void alwaysFail2()
