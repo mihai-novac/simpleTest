0f6451e4456e24b8b980e22398e9c8ce710b44d5
iulia-andreicut <iulia.andreicut@microfocus.com>
2021-02-02 18:16:31 +0200

    Update and rename TestModifiedName.java to ModifiedNameTest.java

--- src/test/java/ModifiedNameTest.java
+++ src/test/java/ModifiedNameTest.java
@@ -9,7 +9,7 @@ import java.util.Random;
                 /**  
                  * Created by dshmaya on 15/11/2017 ...04
                 */  
-                public class TestModifiedName {
+                public class ModifiedNameTest{
                     private int a = 1;
                     private int b =1;
                  
