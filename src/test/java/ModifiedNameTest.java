d0ad2649e2134554b83efc09e4d69ce2601027bc
iulia-andreicut <iulia.andreicut@microfocus.com>
2021-03-16 13:20:25 +0200

    Update ModifiedNameTest.java

--- src/test/java/ModifiedNameTest.java
+++ src/test/java/ModifiedNameTest.java
@@ -96,7 +96,7 @@ import java.util.Random;
                     @Test
                     public void alwaysPassF()
                     {
-                        Assert.assertEquals(1, 1);
+                        Assert.assertEquals(1, 2);
                     }
                    @Test
                    public void alwaysFail5()
