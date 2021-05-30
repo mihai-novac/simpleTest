d1407d34ac3c8416c1e4b582b68873d878eecf3a
iulia-andreicut <iulia.andreicut@microfocus.com>
2021-05-30 12:02:05 +0300

    defect #2002 this  is a test

--- src/test/java/CalcsTest.java
+++ src/test/java/CalcsTest.java
@@ -10,7 +10,7 @@ public class CalcsTest {
     @Test
     public void alwaysFail1()
     {
-        Assert.assertEquals(1, 3);
+        Assert.assertEquals(1, 4);
     }
     @Test
     public void alwaysFail2()
