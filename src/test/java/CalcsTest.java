9710d020af0e1f809d43a79868f67a7545f052f8
iulia-andreicut <iulia.andreicut@microfocus.com>
2021-06-24 15:22:49 +0300

    user story #1249193 automation coverage test

--- src/test/java/CalcsTest.java
+++ src/test/java/CalcsTest.java
@@ -10,7 +10,7 @@ public class CalcsTest {
     @Test
     public void alwaysFail1()
     {
-        Assert.assertEquals(1, 2);
+        Assert.assertEquals(1, 3);
     }
     @Test
     public void alwaysFail2()
