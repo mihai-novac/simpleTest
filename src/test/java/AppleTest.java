725391aa4bde0674049e94b3ccf0eb897c6f272e
iulia-andreicut <iulia.andreicut@microfocus.com>
2021-10-14 08:16:07 +0300

    defect #2097 gtr

---
+++ src/test/java/AppleTest.java
@@ -0,0 +1,24 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AppleTest{
+    @Test
+    public void alwaysFailAp()
+    {
+        Assert.assertEquals(1, 2);
+    }
+    @Test
+    public void alwaysFailAp1()
+    {
+        Assert.assertEquals(1, 2);
+    }
+    @Test
+    public void alwaysFailAp2()
+    {
+        Assert.assertEquals(1, 1);
+    }
+    @Test
+    public void alwaysFailAp3()
+    {
+        Assert.assertEquals(1, 2);
+    }
