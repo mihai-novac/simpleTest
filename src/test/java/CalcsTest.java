ed11b96d48de8476cb6a8b3702092dada92c1609
Mihai Novac <mihai.novac@microfocus.com>
2021-10-18 19:16:12 +0300

    Defect #1372075 safe commit

--- src/test/java/CalcsTest.java
+++ src/test/java/CalcsTest.java
@@ -18,17 +18,17 @@ public class CalcsTest {
         Assert.assertEquals(1, 2);
     }
      @Test
-    public void ESInProgress()
-    {
-        Assert.assertEquals(1, 3);
-    }
-      @Test
-    public void testestestestest()
-    {
+     public void ESInProgress() {
+         Assert.assertEquals(1, 3);
+     }
+
+    @Test
+    public void testestestestesttest() {
         Assert.assertEquals(1, 1);
     }
+
     @Test
-	public void sum_test_A() {
-	Assert.assertEquals(3, VerifyDefect.sum(1,2));
-	}
+    public void sum_test_A() {
+        Assert.assertEquals(3, VerifyDefect.sum(1, 2));
+    }
 }
