1db778b377536676d2a1704ef89adee7bbaba126
Mihai Novac <mihai.novac@microfocus.com>
2021-10-19 13:09:22 +0300

    US #1011 risky

--- src/test/java/AppleTest.java
+++ src/test/java/AppleTest.java
@@ -5,7 +5,7 @@ public class AppleTest{
     @Test
     public void alwaysFailAp()
     {
-        Assert.assertEquals(1, 5);
+        Assert.assertEquals(1, 7);
     }
     @Test
     public void alwaysFailAp1()
