cc22028156eb168698e90aa4483ab5e372d95f12
Mihai Novac <mihai.novac@microfocus.com>
2021-10-19 13:21:50 +0300

    user story #1372073 commit

--- src/test/java/AppleTest.java
+++ src/test/java/AppleTest.java
@@ -5,7 +5,7 @@ public class AppleTest{
     @Test
     public void alwaysFailAp()
     {
-        Assert.assertEquals(1, 8);
+        Assert.assertEquals(1, 4);
     }
     @Test
     public void alwaysFailAp1()
