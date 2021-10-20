478d2f6629059b892b56bf951dcc0b59e600d5ef
Mihai Novac <mihai.novac@microfocus.com>
2021-10-20 19:32:23 +0300

    defect #5100 second commit

--- src/test/java/AppleTest.java
+++ src/test/java/AppleTest.java
@@ -5,7 +5,7 @@ public class AppleTest{
     @Test
     public void alwaysFailAp()
     {
-        Assert.assertEquals(1, 3);
+        Assert.assertEquals(1, 5);
     }
     @Test
     public void alwaysFailAp1()
