c688ae5c7054971602edd025764eb3722ead7d16
Mihai Novac <mihai.novac@microfocus.com>
2021-11-01 16:56:35 +0200

    Defect #1394001 second

--- src/test/java/AppleTest.java
+++ src/test/java/AppleTest.java
@@ -5,7 +5,7 @@ public class AppleTest{
     @Test
     public void alwaysFailAp()
     {
-        Assert.assertEquals(1, 23);
+        Assert.assertEquals(1, 24);
     }
     @Test
     public void alwaysFailAp1()
