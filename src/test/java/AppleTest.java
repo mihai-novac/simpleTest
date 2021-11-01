a7d091f5efe8a182fbd1a5789601324bf545a6d1
Mihai Novac <mihai.novac@microfocus.com>
2021-11-01 16:28:47 +0200

    Defect #2095 fail

--- src/test/java/AppleTest.java
+++ src/test/java/AppleTest.java
@@ -5,7 +5,7 @@ public class AppleTest{
     @Test
     public void alwaysFailAp()
     {
-        Assert.assertEquals(1, 10);
+        Assert.assertEquals(1, 11);
     }
     @Test
     public void alwaysFailAp1()
